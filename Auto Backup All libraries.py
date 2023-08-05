5baa663977fc61aa9d32028a4b784d491ce67d4c
Rixon404 <61840468+Rixon404@users.noreply.github.com>
2023-08-04 20:00:02 -0400

    Auto Backup All libraries v1.1.py
    Now dont repeat the same library.whl downloaded already, just the news.

--- Auto Backup All libraries.py
+++ Auto Backup All libraries.py
@@ -1,29 +1,24 @@
-import os
 import subprocess
-import shutil
+import os
 
-# Ejecutar el comando 'pip list' y capturar la salida
-pip_list_output = subprocess.check_output(["pip", "list"]).decode("utf-8")
+# Ruta de la carpeta de respaldo de .whl
+backup_folder = "/ruta/del/respaldo/de/librerias"
 
-# Divide la salida en líneas y omite las primeras 2 líneas (encabezados)
-pip_list_lines = pip_list_output.strip().split("\n")[2:]
+# Obtener la lista de librerías instaladas
+completed_process = subprocess.run(["pip", "list"], capture_output=True, text=True)
+installed_libraries = [line.split()[0] for line in completed_process.stdout.splitlines()[2:]]
 
-# Crear una carpeta de respaldo si no existe
-backup_folder = "/path/to/the/folder/of/whl"
-os.makedirs(backup_folder, exist_ok=True)
+# Obtener la lista de archivos .whl en la carpeta de respaldo
+existing_whl_files = [filename for filename in os.listdir(backup_folder) if filename.endswith(".whl")]
 
-# Recorre las líneas de la lista y obtiene los nombres y versiones de las librerías
-for line in pip_list_lines:
-    package, version = line.split(maxsplit=1)
-    
-    # Generar el nombre del archivo .whl
-    whl_file = f"{package}-{version}.whl"
-    
-    try:
-        # Copiar el archivo .whl a la carpeta de respaldo
-        subprocess.check_call(["pip", "download", "--dest", backup_folder, package])
-        print(f"Archivo {whl_file} copiado a la carpeta de respaldo.")
-    except subprocess.CalledProcessError:
-        print(f"Error al copiar el archivo {whl_file}.")
+# Crear respaldo solo de librerías no presentes en la carpeta de respaldo
+for library in installed_libraries:
+    library_name = library.split()[0]
+    if not any(library_name in filename for filename in existing_whl_files):
+        try:
+            subprocess.run(["pip", "download", "--no-deps", "-d", backup_folder, library_name])
+            print(f"Respaldo de {library_name} creado correctamente.")
+        except subprocess.CalledProcessError:
+            print(f"Error al crear el respaldo de {library_name}.")
 
-print("Proceso completado.")
+print("Proceso de respaldo completado.")
